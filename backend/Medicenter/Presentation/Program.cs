b3fa6c818e9aad4a63b11eb3f7650d15e3c9582b
facundoMandon <facundo-mandon@hotmail.com>
2025-11-03 13:35:16 -0300

    configuramos el workflow de github actions

--- backend/Medicenter/Presentation/Program.cs
+++ backend/Medicenter/Presentation/Program.cs
@@ -103,14 +103,11 @@ builder.Services.AddSwaggerGen(setupAction =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API V1");
-    });
-}
+    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
